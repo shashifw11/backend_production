9112c8ef145a4c543b88513ea66a6d09c0bb16f7
Shashi <shekhar.shashi735@gmail.com>
2026-06-09 01:03:21 +0530

    all set up completed

--- src/app.js
+++ src/app.js
@@ -19,6 +19,15 @@ app.use(express.urlencoded({  // if url comes in request
 
 app.use(cookieParser());
 
+// routes import 
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+// app.use("/users" , userRouter);
+// http://localhost:8000/users
+app.use("/api/v1/users" , userRouter);
+
 
 
 
